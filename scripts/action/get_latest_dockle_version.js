851507c73c5ab7825c25d4375cb1e10548082dc6
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-08-07 08:59:12 +0900

    super-linterによる指摘事項修正

--- scripts/action/get_latest_dockle_version.js
+++ scripts/action/get_latest_dockle_version.js
@@ -1,7 +1,7 @@
 module.exports = async ({ github }) => {
   const release = await github.rest.repos.getLatestRelease({
-    owner: 'goodwithtech',
-    repo: 'dockle'
-  })
-  return release.data.tag_name.replace('v', '')
-}
+    owner: "goodwithtech",
+    repo: "dockle",
+  });
+  return release.data.tag_name.replace("v", "");
+};
