41bac6da0a561309396fd9cee531156201e23d63
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-21 08:54:40 +0900

    CIで使用しているスクリプトをTypeScript化する

--- src/get_latest_dockle_version.ts
+++ src/get_latest_dockle_version.ts
@@ -1,7 +1,10 @@
-module.exports = async ({ github }) => {
-  const release = await github.rest.repos.getLatestRelease({
+import type { GitHub } from "@actions/github/lib/utils";
+import type {RestEndpointMethodTypes} from "@octokit/plugin-rest-endpoint-methods";
+
+export async function script(github: InstanceType<typeof GitHub>):Promise<string> {
+  const release:RestEndpointMethodTypes["repos"]["getLatestRelease"]["response"] = await github.rest.repos.getLatestRelease({
     owner: "goodwithtech",
     repo: "dockle",
   });
   return release.data.tag_name.replace("v", "");
-};
+}
