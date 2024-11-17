990466d8b2151bd013cdf9bb54b872458c541346
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-11-17 14:16:55 +0900

    CIで使用しているスクリプトをTypeScript化する (#1564)
    * js -> ts
    
    * CIで使用しているスクリプトをTypeScript化する
    
    * formatが間違ってたので直してあげたよ！ (#1565)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * package.jsonやpackage-lock.jsonを直してあげたよ！ (#1567)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * dist反映 (#1566)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: dev-hato-app[bot] <111467236+dev-hato-app[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/get_latest_dockle_version.ts
@@ -0,0 +1,13 @@
+import type { GitHub } from "@actions/github/lib/utils";
+import type { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+
+export async function script(
+  github: InstanceType<typeof GitHub>,
+): Promise<string> {
+  const release: RestEndpointMethodTypes["repos"]["getLatestRelease"]["response"] =
+    await github.rest.repos.getLatestRelease({
+      owner: "goodwithtech",
+      repo: "dockle",
+    });
+  return release.data.tag_name.replace("v", "");
+}
